3bdaac19f5b9f6c66ac48776dacf8f2a021a2b7f
Youran <sengyouran2001@gmail.com>
2025-08-04 17:26:34 +0700

    Remove unused Report page and route

--- src/page/index.js
+++ src/page/index.js
@@ -1,4 +1,3 @@
 export { default as Dashboard } from "../page/Dashboard";
 export { default as AboutUs } from "../page/AboutUs";
 export { default as Customer } from "../page/Customer";
-export { default as Report } from "../page/Report";
